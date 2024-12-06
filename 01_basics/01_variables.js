8347e3282e91b4d7c638e3b0dad34e06b01a8806
AbhinaySingh15 <abhinaysingh640@gmail.com>
2024-12-06 00:11:37 +0000

    we done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "abhinay@gmail.com"
+var accountpassword ="23456"
+accountCity = "Bhopal"
+
+/*
+never ever use var
+*/
+
+accountEmail = "ah@gmail.com"
+accountPassword = "212121"
+accountCity = "Satna"
+
+console.log("accountId");
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
