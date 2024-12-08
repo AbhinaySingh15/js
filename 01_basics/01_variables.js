27bd5e1bbee05c788aa22a576c5a3afa405660f7
AbhinaySingh15 <abhinaysingh640@gmail.com>
2024-12-08 06:17:27 +0000

    code done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,10 @@ const accountId = 12345
 let accountEmail = "abhinay@gmail.com"
 var accountpassword ="23456"
 accountCity = "Bhopal"
-
+let accountState;
 /*
 never ever use var
+because of issue in use of block scope and functional scope
 */
 
 accountEmail = "ah@gmail.com"
@@ -13,4 +14,4 @@ accountCity = "Satna"
 
 console.log("accountId");
 
-console.table([accountId, accountEmail, accountPassword,accountCity])
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+// two documentation : tc39(ecma) and mdn
+
+let name = "abhinay"
+let age = 19
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null => standalone value 
+// undefined 
+// symbol => unique 
+
+// object
+
+console.log(typeof "Abhinay") ;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
