96e86c922cb53df96ae7a090215306e4a60561f6
AbhinaySingh15 <abhinaysingh640@gmail.com>
2024-12-26 06:07:24 +0000

    datatype done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// while comparing keep focus on that same datatypes are being compared.
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null> 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// js me comparision aur equality check (==) dono alag alag hai .
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// all conversion from line 9 to line 20 can make you confuse . avoid it.#cleanCode
+
+// strict check (===), here it started checking datatype also.
+
+console.log("2"===2);
