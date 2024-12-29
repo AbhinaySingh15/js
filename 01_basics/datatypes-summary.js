eccf67785b2f57061e3f8db612e01242642d9211
AbhinaySingh15 <abhinaysingh640@gmail.com>
2024-12-29 10:35:06 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,1 +1,22 @@
- 
+ // ager apko js master kerni hai to do jiz ko master ker lo ; objects and browser events .
+
+ // Primitive 
+
+ // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ console.log(id === anotherId);
+
+ const bigNumber = 34251678n
+
+
+
+ // Reference (non primitive)
+
+ // Array, Objects, Functions
